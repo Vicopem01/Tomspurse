f338b6ea62adcfb05220f841c9973574ee6cc7c3
Vicopem01 <vicopem@gmail.com>
2021-06-04 10:57:20 +0100

    bug fixes

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let menu = document.querySelector("#menu-icon");
 let ulTop = document.querySelector(".ul-top");
+let remove = document.querySelector(".remove")
+
 
 let show = false;
 function popUp() {
     console.log(true)
   if (show) {
+    remove.style.display = "block"
     ulTop.style.display = "none";
     show= false
   } else {
     ulTop.style.display = "block";
+    remove.style.display = "none"
     show = true
   }
 }
