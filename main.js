f7923fae88752b48920083690b73db77bee761a1
mayowa dabiri <mayowad43@gmai.com>
2021-06-04 10:44:03 +0100

    bug fixes

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-let menu = document.querySelector("#menu-icon")
-let ulTop= document.querySelector(".ul-top")
+let menu = document.querySelector("#menu-icon");
+let ulTop = document.querySelector(".ul-top");
+
+let show = false;
 function popUp() {
+    console.log(true)
+  if (show) {
+    ulTop.style.display = "none";
+    show= false
+  } else {
     ulTop.style.display = "block";
+    show = true
+  }
 }
 menu.addEventListener("click", popUp);
